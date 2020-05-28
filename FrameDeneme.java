5b638ae07aad39ba921a6a6738e260c00449b0a9
aberksarac <65914076+aberksarac@users.noreply.github.com>
2020-05-28 21:21:33 +0300

    Add files via upload

---
+++ FrameDeneme.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+
+public class FrameDeneme {
+
+
+	
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.add(new CheckBoxPaneli());
+		frame.setVisible(true);
+		frame.setBounds(100, 100, 200, 150);
+
+	}
+
+}
